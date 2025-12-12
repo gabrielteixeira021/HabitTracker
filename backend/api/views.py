437a70476521c28cf76052aaa1f75d9e0e66fc89
gabrielTeixeira0021 <charlesgabriel102@gmail.com>
2025-12-12 20:57:46 -0300

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+
+# from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import UserSerializer
+
+
+# Create your views here.
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
