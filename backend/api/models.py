fc88719a8d3e935fec4042ba87f64bf28a7a8f54
gabrielTeixeira0021 <charlesgabriel102@gmail.com>
2025-12-14 17:36:25 -0300

    feat: Habits model added

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Habit(models.Model):
+    FREQUENCY_CHOICES = [
+        ("daily", "Daily"),
+        ("weekly", "Weekly"),
+        ("monthly", "Monthly"),
+    ]
+
+    name = models.CharField(max_length=200)
+    frequency = models.CharField(
+        choices=FREQUENCY_CHOICES,
+        max_length=10,
+    )
+    current_streak = models.IntegerField(default=0)  # pyright: ignore[reportArgumentType]
+    best_streak = models.IntegerField(default=0)  # pyright: ignore[reportArgumentType]
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="habits")
+
+
+def __str__(self):
+    return self.name
